b88fdd5dace94d1cd856bbb1d9df0f7ebb8a28dd
Alex <aleksejtatukov098@gmail.com>
2023-07-04 22:17:49 +0300

    task25

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+int third = first;
+if (second == 1)
+{
+    first *= second;
+    Console.WriteLine(first);
+}
+for (int i = 1; i < second; i++)
+{
+
+
+    first *= third;
+    Console.WriteLine(first);
+}
